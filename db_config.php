d58845de8be9b60b64ad3995cb2c6d24d6b06754
Mizuno Kanta <st042103@m03.kyoto-kcg.ac.jp>
2024-12-12 20:13:04 +0900

    UPDATE db_config.php

--- db_config.php
+++ db_config.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 $servername = "localhost";  // サーバーホスト名（通常は "localhost"）
 $username = "root";         // MySQLユーザー名（デフォルトは "root"）
@@ -12,4 +13,20 @@ if ($conn->connect_error) {
     die("データベース接続に失敗しました: " . $conn->connect_error);
 }
 
+=======
+<?php
+$servername = "localhost";  // サーバーホスト名（通常は "localhost"）
+$username = "root";         // MySQLユーザー名（デフォルトは "root"）
+$password = "";             // MySQLパスワード（デフォルトは空）
+$dbname = "test";           // データベース名
+
+// データベース接続
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 接続エラーチェック
+if ($conn->connect_error) {
+    exit("データベース接続に失敗しました:". $conn->connect_error);
+}
+
+>>>>>>> 73ef010 (UPDATE db_config.php)
 ?>
